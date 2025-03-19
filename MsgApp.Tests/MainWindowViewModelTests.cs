1d3a432aae2f02d30e0a5f8e1427f424ef4e2d52
vasi <b.avg@hotmail.com>
2025-03-19 16:53:18 +0100

    Erweitern der Testabdeckung

---
+++ MsgApp.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,118 @@
+using NUnit.Framework;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MsgApp.Models;
+using MsgApp.Services;
+using MsgApp.ViewModels;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace MsgApp.Tests
+{
+    [TestFixture]
+    public class MainWindowViewModelTests
+    {
+      private MainWindowViewModel _viewModel;
+      private MockTimer _fakeTimer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeTimer = new MockTimer();
+            var httpClientService = new MockHttpClientService();
+            var messageStateService = new MessageStateService(NullLogger<MessageStateService>.Instance, _fakeTimer);
+            var messageLoader = new JsonMessageLoader(NullLogger<JsonMessageLoader>.Instance);
+            var gravatarService = new GravatarService(NullLogger<GravatarService>.Instance, httpClientService);
+            _viewModel = new MainWindowViewModel(messageLoader, NullLogger<MainWindowViewModel>.Instance, messageStateService, gravatarService);
+        }
+
+        [Test]
+        public void TestSortBySender_SortierungNachSenderName()
+        {
+            // Arrange: unsortierte Nachrichtenliste
+            _viewModel.Messages = new ObservableCollection<Message>
+            {
+                new Message { SenderName = "Zoe" },
+                new Message { SenderName = "Anna" },
+                new Message { SenderName = "Mike" }
+            };
+
+            // Act
+            _viewModel.SortBySender();
+
+            // Assert
+            var sorted = _viewModel.Messages.ToList();
+            Assert.That(sorted[0].SenderName, Is.EqualTo("Anna"));
+            Assert.That(sorted[1].SenderName, Is.EqualTo("Mike"));
+            Assert.That(sorted[2].SenderName, Is.EqualTo("Zoe"));
+        }
+
+        [Test]
+        public void TestSortByDate_SortierungNachDatum()
+        {
+            // Arrange: unsortierte Nachrichten Datums
+            _viewModel.Messages = new ObservableCollection<Message>
+            {
+                new Message { SentDate = new DateTime(2025, 3, 2) },
+                new Message { SentDate = new DateTime(2025, 3, 1) },
+                new Message { SentDate = new DateTime(2025, 3, 3) }
+            };
+
+            // Act
+            _viewModel.SortByDate();
+
+            // Assert: Überprüfe die chronologische Sortierung
+            var sorted = _viewModel.Messages.ToList();
+            Assert.That(sorted[0].SentDate, Is.EqualTo(new DateTime(2025, 3, 1)));
+            Assert.That(sorted[1].SentDate, Is.EqualTo(new DateTime(2025, 3, 2)));
+            Assert.That(sorted[2].SentDate, Is.EqualTo(new DateTime(2025, 3, 3)));
+        }
+
+        [Test]
+        public void TestSelectedMessage_StartetMarkAsReadTimer()
+        {
+            // Arrange: Erstelle eine Testnachricht
+            var msg = new Message { SenderName = "Test", IsRead = false };
+            _viewModel.Messages = new ObservableCollection<Message> { msg };
+
+            // Act: Setze SelectedMessage
+            _viewModel.SelectedMessage = msg;
+
+            // Assert: Überprüfe, dass unser FakeTimerService (als Teil des MessageStateService)
+            // den DelayAsync-Aufruf registriert hat.
+            Assert.That(_fakeTimer.CallCount, Is.EqualTo(1));
+        }
+    }
+
+    // Fake JSON-Loader, der eine Dummy-Nachrichtenliste liefert
+    // public class FakeJsonMessageLoader : JsonMessageLoader
+    // {
+    //     public FakeJsonMessageLoader() : base(NullLogger<JsonMessageLoader>.Instance) { }
+    //     public new List<Message> LoadMessagesFromJson(string path)
+    //     {
+    //         // Liefert eine Dummy-Liste (hier z. B. eine leere Liste)
+    //         return new List<Message>();
+    //     }
+    // }
+
+    // // Fake GravatarService, der keine echten Avatare lädt
+    // public class FakeGravatarService : GravatarService
+    // {
+    //     public FakeGravatarService() : base(NullLogger<GravatarService>.Instance, new FakeHttpClientService()) { }
+    //     public new Task LoadAvatarAsync(Message msg)
+    //     {
+    //         // Dummy-Verhalten: Kein Avatar wird geladen
+    //         msg.AvatarBitmap = null;
+    //         return Task.CompletedTask;
+    //     }
+    // }
+
+    // // Fake HttpClientService (nur als Platzhalter)
+    // public class FakeHttpClientService : HttpClientService
+    // {
+    //     public FakeHttpClientService() : base() { }
+    // }
+}
--- MsgApp.Tests/MockServices/MockTimer.cs
+++ MsgApp.Tests/MockServices/MockTimer.cs
@@ -4,7 +4,7 @@ using MsgApp.Services;
 public class MockTimer : ITimerService
 {
     //  Zähler um zu sehen, ob DelayAsync aufgerufen wurde.
-    public int CallCount { get; private set; }
+    public int CallCount { get; private set; }=0;
 
     public Task DelayAsync(TimeSpan delay, CancellationToken token)
     {
